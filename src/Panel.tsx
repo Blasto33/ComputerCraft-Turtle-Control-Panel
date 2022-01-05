4eed6823d0908a3bfa400d9dcb43acaca897c8a7
blasto33 <thomas.de-barros@outlook.fr>
2022-01-05 03:20:21 +0100

    Initial commit, creating webpage using React and TypeScript

---
+++ src/Panel.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Panel.css';
+
+function Panel() {
+  return (
+    <div className="Panel">
+      <h1>ComputerCraft Turtle Control Panel</h1>
+    </div>
+  );
+}
+
+export default Panel;
